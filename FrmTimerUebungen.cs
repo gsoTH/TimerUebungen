d67ded6ea5b947f405784e5d7ef49640497c3837
M.Thomas <th@gso-koeln.de>
2022-02-18 09:04:28 +0100

    Projektdateien hinzufügen.

---
+++ FrmTimerUebungen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TimerUebungen
+{
+    public partial class FrmTimerUebungen : Form
+    {
+        Timer timer;
+
+        public FrmTimerUebungen()
+        {
+            InitializeComponent();
+
+            TimerEinstellenUndStarten();
+
+        }
+
+        private void TimerEinstellenUndStarten()
+        {
+            //Timer initialisieren, er ist noch nicht gestartet
+            timer = new Timer();
+
+            //Zeit zwischen Ereignissen in ms festlegen
+            timer.Interval = 10;
+
+            // Methode zuweisen, die ausgeführt wird, wenn der Intervall abgelaufen ist.
+            timer.Tick += new EventHandler(TickEreignis); //Hier wird ein Delegat erzeugt.
+
+            timer.Start();
+        }
+
+        private void TickEreignis(Object myObject, EventArgs myEventArgs)
+        { 
+           // Code einfügen, der nach jedem Tick ausgeführt werden soll.
+        }
+
+    }
+}
