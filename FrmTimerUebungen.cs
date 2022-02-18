4505488b4f33fb552e4fc7c7e7765aa703a71afd
M.Thomas <th@gso-koeln.de>
2022-02-18 10:20:17 +0100

    System.Drawing einbinden.
    System. Drawing muss eingebunden werden, damit wir auf GDI+ zugreifen können. Ansonsten kann die Klasse Rectangle nicht gefunden werden.
    Durch das Deklarieren des Rectangles auf Klassenebene statt innerhalb einer Methode, behält es seinen Zustand auch nach Ende z.B. des Paint-Ereignisses.

--- FrmTimerUebungen.cs
+++ FrmTimerUebungen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace TimerUebungen
@@ -6,6 +7,7 @@ namespace TimerUebungen
     public partial class FrmTimerUebungen : Form
     {
         Timer timer;
+        Rectangle rect;
 
         public FrmTimerUebungen()
         {
